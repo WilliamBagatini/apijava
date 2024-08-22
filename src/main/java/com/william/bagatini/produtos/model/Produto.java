daa2c4af05d5451a005fa75cbe7584e7c46a1a81
WilliamBagatini <154237674+WilliamBagatini@users.noreply.github.com>
2024-08-22 19:56:21 -0300

    Api java springboot

---
+++ src/main/java/com/william/bagatini/produtos/model/Produto.java
@@ -0,0 +1,28 @@
+package com.william.bagatini.produtos.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String nome;
+    @Column(nullable = false)
+    private double preco;
+
+}
